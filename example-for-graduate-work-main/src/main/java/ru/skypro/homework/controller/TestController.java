b48afa5e9c25413e415a6d07e5c66f5fee41c708
BuenosDiasGente <morkovka03121981@gmail.com>
2023-12-05 14:32:38 +0300

    refactoring coda

---
+++ example-for-graduate-work-main/src/main/java/ru/skypro/homework/controller/TestController.java
@@ -0,0 +1,21 @@
+package ru.skypro.homework.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.skypro.homework.model.User;
+import ru.skypro.homework.service.UserService;
+import ru.skypro.homework.service.impl.UserServiceImpl;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    private final UserServiceImpl userService;
+
+    @GetMapping("/test")
+    public String test(@RequestParam String username) {
+       return userService.findUserByLoginWithCriteria(username).toString();
+    }
+}
